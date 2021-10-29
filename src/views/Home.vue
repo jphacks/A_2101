68af030f13b2e99f39816ff50ad6b56016d0cc55
ToruTamahashi <tamahashi.toru@nowhere.co.jp>
2021-10-30 04:43:52 +0900

    余計なコードの削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Set up success github actions!" />
-  </div>
+  <div>TopPage</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
